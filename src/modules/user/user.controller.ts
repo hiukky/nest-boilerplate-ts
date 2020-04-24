599d11becf9bd92d6ce257e39ee8ac09c78b7939
Romullo <developermarsh@gmail.com>
2020-04-24 09:40:14 -0300

    chore(modules): simple crud created

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,14 +1,38 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Post, Put, Body, Param, Delete } from '@nestjs/common'
 
 import { UserService } from './user.service'
 import { UserEntity } from './user.entity'
+import { UserDto } from './dto/user.dto'
 
-@Controller()
+@Controller({ path: 'users' })
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get('users')
+  @Get()
   async index(): Promise<UserEntity[]> {
     return await this.userService.findAll()
+  }
+
+  @Get(':id')
+  async show(@Param() id: number): Promise<UserEntity> {
+    return await this.userService.findOne(id)
+  }
+
+  @Post()
+  async store(@Body() userDto: UserDto): Promise<UserEntity> {
+    return this.userService.create(userDto)
+  }
+
+  @Put(':id')
+  async update(
+    @Body() userDto: UserDto,
+    @Param() id: number,
+  ): Promise<UserEntity> {
+    return this.userService.update(id, userDto)
+  }
+
+  @Delete(':id')
+  async remove(@Param() id: number): Promise<number> {
+    return await this.userService.delete(id)
   }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -3,6 +3,8 @@ import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 
 import { UserEntity } from './user.entity'
+import { UserDto } from './dto/user.dto'
+
 @Injectable()
 export class UserService {
   constructor(
@@ -10,7 +12,24 @@ export class UserService {
     private readonly userRepository: Repository<UserEntity>,
   ) {}
 
+  async findOne(id: number): Promise<UserEntity> {
+    return await this.userRepository.findOne(id)
+  }
+
   async findAll(): Promise<UserEntity[]> {
     return await this.userRepository.find()
+  }
+
+  async create(userDto: UserDto): Promise<UserEntity> {
+    return await this.userRepository.create(userDto).save()
+  }
+
+  async update(id: number, userDto: UserDto): Promise<UserEntity> {
+    await this.userRepository.update(id, userDto)
+    return await this.userRepository.findOne(id)
+  }
+
+  async delete(id: number): Promise<number> {
+    return (await this.userRepository.delete(id)).raw
   }
 }
